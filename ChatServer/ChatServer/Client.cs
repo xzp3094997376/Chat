e60b885be1477b610b60731af1baf0274693067c
xzp <3094997376@qq.com>
2019-06-02 21:35:21 +0800

    first

---
+++ ChatServer/ChatServer/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ChatServer
+{
+    //处理客户端通信
+    class Client
+    {
+        private Socket mSoket;
+        Thread t;
+        byte[] data = new byte[1024];
+        public Client(Socket s)
+        {
+            mSoket=s;
+            t = new Thread(ReceiveMsg);
+            t.Start();
+        }
+        private void ReceiveMsg()
+        {
+            while (true)
+            {
+                if (mSoket.Poll(10,SelectMode.SelectRead))
+                {
+                    mSoket.Close();
+                    mSoket.Close();
+                    Console.WriteLine("客户端断开");
+                    break;
+                }
+                int length= mSoket.Receive(data);
+                string msg = Encoding.UTF8.GetString(data, 0, length);           
+                //TODO:收到数据分发到客户端
+                Console.WriteLine("收到消息:  "+msg);
+                Program.BroadcastMsg(msg);
+            }
+        }
+
+       public void SendMsg(string msg)
+        {
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes(msg);
+            mSoket.Send(data);
+            Console.WriteLine(msg + "   send to client");
+        }
+        public bool Conected
+        {
+            get
+            {
+                return mSoket.Connected;
+            }
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> clientList = new List<Client>();
+        static void Main(string[] args)
+        {
+            Socket serverSockets = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSockets.Bind(new IPEndPoint(IPAddress.Parse("192.168.137.1"), 8888));
+            serverSockets.Listen(100);
+            Console.WriteLine("listen:   ");
+            while (true)
+            {
+                //Console.WriteLine("监听到信息：");
+                Socket client= serverSockets.Accept();
+                Console.WriteLine("client connetcted");
+                Client mClient = new Client(client);
+                clientList.Add(mClient);                                   
+            }             
+        }
+        public static void BroadcastMsg(string msg)
+        {
+            var notConnectList = new List<Client>();
+            foreach (var item in clientList)
+            {
+                if (item.Conected)
+                {                    
+                    item.SendMsg(msg);
+                }
+                else
+                {
+                    notConnectList.Add(item);
+                }             
+            }
+            foreach (var item in notConnectList)
+            {
+                clientList.Remove(item);
+            }
+        }
+    }
+}
